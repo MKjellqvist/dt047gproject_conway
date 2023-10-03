7c46201168e08549f0c377ffac10ecd6cb71dd4a
Martin Kjellqvist <martin.kjellqvist@miun.se>
2023-10-03 19:13:44 +0200

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/**
+ * @mainpage
+ * Projektimplementation i DT047G. Conways game of life.
+ *
+ * Ointressant i sin rena form men kan bli intressant.
+ *
+ * Startkonfigurationerna läggs upp i en katalog.
+ */
+int main() {
+
+}
